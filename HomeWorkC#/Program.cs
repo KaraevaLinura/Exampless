47806c96179102a35c94bf1f8d3d0a547a2f0374
LinuraKaraeva <Linura.Karaeva.87@mail.ru>
2022-08-14 17:33:34 +0600

    Третья цифра

--- HomeWorkC#/Program.cs
+++ HomeWorkC#/Program.cs
@@ -89,7 +89,19 @@
 
 // ДЗ 4
 
-Console.Write("Введи трёхзначное число: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+// Console.Write("Введи трёхзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(threeDigitNumber);
+// Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2)
+{
+Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else
+{
+Console.WriteLine("-> третьей цифры нет");
+}
