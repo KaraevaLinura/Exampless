3238145db4906e3f5cee84a389527e43b100ae36
LinuraKaraeva <Linura.Karaeva.87@mail.ru>
2022-08-14 17:15:36 +0600

    ДЗ максимальное из трех чисел

--- HomeWorkC#/Program.cs
+++ HomeWorkC#/Program.cs
@@ -1,4 +1,35 @@
-﻿// Домашняя работа
+﻿// // Домашняя работа Максимальное из двух чисел
+// namespace HelloApp
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Введите первое число: ");
+//             int num1 = Convert.ToInt32(Console.ReadLine());
+//             Console.WriteLine("Введите второе число: ");
+//             int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+//             if(num1 > num2)
+//             {
+//                 Console.Write("max = ");
+//                 Console.Write(num1);
+//             }
+//             else if (num1 < num2)
+//             {
+//                 Console.Write("max = ");
+//                 Console.Write(num2);
+//             }
+//             else
+//             {
+//                 Console.WriteLine("Оба числа равны");
+//             }
+//             Console.ReadKey();
+//         }
+//     }
+// }
+// ДЗ 2
+
 namespace HelloApp
 {
     class Program
@@ -6,24 +37,25 @@ namespace HelloApp
         static void Main(string[] args)
         {
             Console.WriteLine("Введите первое число: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите второе число: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
- 
-            if(num1 > num2)
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+             
+            if(a > b && a > c)
             {
-                Console.Write("max = ");
-                Console.Write(num1);
+                Console.WriteLine(a);
             }
-            else if (num1 < num2)
+            else if (b > a && b > c)
             {
-                Console.Write("max = ");
-                Console.Write(num2);
+                Console.WriteLine(b);
             }
             else
             {
-                Console.WriteLine("Оба числа равны");
+                Console.WriteLine(c);
             }
+            
             Console.ReadKey();
         }
     }
